5c0f5d258a9a531a0462c585e88c14d24eef5c16
Darren Shepherd <darren@rancher.com>
2018-08-04 22:51:55 -0700

    Update vendor

--- vendor/k8s.io/kubernetes/pkg/generated/openapi/zz_generated.openapi.go
+++ vendor/k8s.io/kubernetes/pkg/generated/openapi/zz_generated.openapi.go
@@ -8394,7 +8394,7 @@ func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenA
 						},
 						"fsType": {
 							SchemaProps: spec.SchemaProps{
-								Description: "Filesystem type to mount. Must be a filesystem type supported by the host operating system. Ex. \"ext4\", \"xfs\", \"ntfs\". Implicitly inferred to be \"ext4\" if unspecified.",
+								Description: "Filesystem type to mount. Must be a filesystem type supported by the host operating system. Ex. \"ext4\", \"xfs\", \"ntfs\".",
 								Type:        []string{"string"},
 								Format:      "",
 							},
