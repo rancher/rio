4358abd86082faea22e2884471738119170883a4
Darren Shepherd <darren@rancher.com>
2018-08-09 09:13:21 -0700

    Check that podIP==hostIP==label on gateway

--- controllers/backend/pod/pod_ip_changed.go
+++ controllers/backend/pod/pod_ip_changed.go
@@ -1,6 +1,9 @@
 package pod
 
-import "k8s.io/api/core/v1"
+import (
+	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
+)
 
 const (
 	hostIPLabel = "rio.cattle.io/pod-ip"
@@ -17,7 +20,9 @@ func (p *Controller) checkChangedIP(pod *v1.Pod) (bool, error) {
 		pod.Labels[hostIPLabel] = pod.Status.PodIP
 		_, err := p.pods.Update(pod)
 		return false, err
-	} else if ip != pod.Status.PodIP {
+	} else if ip != pod.Status.PodIP || ip != pod.Status.HostIP {
+		logrus.Infof("Deleting gateway %s because pod IP changed %s=>(%s/%s)", pod.Name, ip, pod.Status.PodIP,
+			pod.Status.HostIP)
 		err := p.pods.Delete(pod.Name, nil)
 		return false, err
 	}
