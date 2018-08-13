07a07f1e8534eee9f6634dc463e2f96733613a31
Darren Shepherd <darren@rancher.com>
2018-08-13 12:59:17 -0700

    Fix service LB issues
    Don't set LB to zero ports.
    Try hostname == "localhost" as 127.0.0.1

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -10,9 +10,9 @@ import (
 	"github.com/rancher/rio/types"
 	"github.com/rancher/rio/types/apis/rio.cattle.io/v1beta1"
 	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/kubernetes/staging/src/k8s.io/apimachinery/pkg/api/errors"
 )
 
 func AddData(rContext *types.Context, inCluster bool) error {
