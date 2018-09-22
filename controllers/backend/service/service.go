cc886b7aca700f0e36c7221e77b408533a6f808f
Daishan Peng <daishan@rancher.com>
2018-09-21 22:12:20 -0700

    make volume works and other fixes

--- controllers/backend/service/service.go
+++ controllers/backend/service/service.go
@@ -26,15 +26,18 @@ var (
 func Register(ctx context.Context, rContext *types.Context) {
 	s := &serviceController{
 		deploymentLister:  rContext.Apps.Deployments("").Controller().Lister(),
+		statefulSetLister: rContext.Apps.StatefulSets("").Controller().Lister(),
 		serviceController: rContext.Rio.Services("").Controller(),
 		services:          rContext.Rio.Services(""),
 	}
 
 	s.serviceController.AddHandler("service-controller", s.sync)
 	rContext.Apps.Deployments("").AddHandler("service-controller", s.deploymentChanged)
+	rContext.Apps.StatefulSets("").AddHandler("service-controller", s.statefulSetChanged)
 }
 
 type serviceController struct {
+	statefulSetLister v1beta2.StatefulSetLister
 	deploymentLister  v1beta2.DeploymentLister
 	serviceController v1beta1.ServiceController
 	services          v1beta1.ServiceInterface
@@ -47,6 +50,20 @@ func (s *serviceController) deploymentChanged(key string, deployment *appsv1beta
 
 	service := deployment.Labels["rio.cattle.io/service"]
 	ns := deployment.Labels["rio.cattle.io/namespace"]
+	if ns != "" && service != "" {
+		s.serviceController.Enqueue(ns, service)
+	}
+
+	return nil
+}
+
+func (s *serviceController) statefulSetChanged(key string, statefulSet *appsv1beta2.StatefulSet) error {
+	if statefulSet == nil {
+		return nil
+	}
+
+	service := statefulSet.Labels["rio.cattle.io/service"]
+	ns := statefulSet.Labels["rio.cattle.io/namespace"]
 	if ns != "" && service != "" {
 		s.serviceController.Enqueue(ns, service)
 	}
@@ -89,34 +106,60 @@ func (s *serviceController) sync(key string, service *v1beta1.Service) error {
 	set["rio.cattle.io/service"] = service.Name
 	set["rio.cattle.io/namespace"] = service.Namespace
 
-	deps, err := s.deploymentLister.List(service.Namespace, set.AsSelector())
-	if err != nil || len(deps) == 0 {
-		return err
-	}
-
 	isUpgrading := false
 	newService := service.DeepCopy()
 
-	for _, dep := range deps {
-		rev := dep.Labels["rio.cattle.io/revision"]
-		if rev == "latest" {
-			newService.Status.DeploymentStatus = &dep.Status
-		} else {
-			if revSpec, ok := newService.Spec.Revisions[rev]; ok {
-				revSpec.Status.DeploymentStatus = &dep.Status
-				newService.Spec.Revisions[rev] = revSpec
+	deps, err := s.deploymentLister.List(service.Namespace, set.AsSelector())
+	if err != nil {
+		return err
+	}
+	if len(deps) != 0 {
+		for _, dep := range deps {
+			rev := dep.Labels["rio.cattle.io/revision"]
+			if rev == "latest" {
+				newService.Status.DeploymentStatus = &dep.Status
+			} else {
+				if revSpec, ok := newService.Spec.Revisions[rev]; ok {
+					revSpec.Status.DeploymentStatus = &dep.Status
+					newService.Spec.Revisions[rev] = revSpec
+				}
+			}
+
+			if upgrading[progressing.GetReason(dep)] || dep.Generation != dep.Status.ObservedGeneration {
+				isUpgrading = true
 			}
 		}
 
-		if upgrading[progressing.GetReason(dep)] || dep.Generation != dep.Status.ObservedGeneration {
-			isUpgrading = true
+		if isUpgrading {
+			updated.Unknown(newService)
+		} else if hasAvailable(newService.Status.DeploymentStatus.Conditions) {
+			newService.Status.Conditions = nil
 		}
-	}
+	} else {
+		stss, err := s.statefulSetLister.List(service.Namespace, set.AsSelector())
+		if err != nil {
+			return err
+		}
+		for _, sts := range stss {
+			rev := sts.Labels["rio.cattle.io/revision"]
+			if rev == "latest" {
+				newService.Status.DeploymentStatus = convertStatefulSetStatus(sts.Status)
+			} else {
+				if revSpec, ok := newService.Spec.Revisions[rev]; ok {
+					revSpec.Status.DeploymentStatus = convertStatefulSetStatus(sts.Status)
+					newService.Spec.Revisions[rev] = revSpec
+				}
+			}
 
-	if isUpgrading {
-		updated.Unknown(newService)
-	} else if hasAvailable(newService.Status.DeploymentStatus.Conditions) {
-		newService.Status.Conditions = nil
+			if upgrading[progressing.GetReason(sts)] || sts.Generation != sts.Status.ObservedGeneration {
+				isUpgrading = true
+			}
+		}
+		if isUpgrading {
+			updated.Unknown(newService)
+		} else {
+			newService.Status.Conditions = nil
+		}
 	}
 
 	if !reflect.DeepEqual(service, newService) {
@@ -125,6 +168,30 @@ func (s *serviceController) sync(key string, service *v1beta1.Service) error {
 	}
 
 	return nil
+}
+
+func convertStatefulSetStatus(status appsv1beta2.StatefulSetStatus) *appsv1beta2.DeploymentStatus {
+	conditions := make([]appsv1beta2.DeploymentCondition, 0)
+	for _, c := range status.Conditions {
+		conditions = append(conditions, convertStatefulSetCondition(c))
+	}
+	return &appsv1beta2.DeploymentStatus{
+		Replicas:          status.Replicas,
+		ReadyReplicas:     status.ReadyReplicas,
+		UpdatedReplicas:   status.UpdatedReplicas,
+		AvailableReplicas: status.CurrentReplicas,
+		Conditions:        conditions,
+	}
+}
+
+func convertStatefulSetCondition(condition appsv1beta2.StatefulSetCondition) appsv1beta2.DeploymentCondition {
+	return appsv1beta2.DeploymentCondition{
+		Type:               appsv1beta2.DeploymentConditionType(string(condition.Type)),
+		Status:             condition.Status,
+		LastTransitionTime: condition.LastTransitionTime,
+		Reason:             condition.Reason,
+		Message:            condition.Message,
+	}
 }
 
 func hasAvailable(cond []appsv1beta2.DeploymentCondition) bool {
