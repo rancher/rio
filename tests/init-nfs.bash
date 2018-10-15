3cce7960a736efe57d56593b6218836806b903d9
Daishan Peng <daishan@rancher.com>
2018-10-15 13:44:29 -0700

    back one space and bring peace

--- tests/init-nfs.bash
+++ tests/init-nfs.bash
@@ -6,7 +6,7 @@ setup() {
     nfs_client_stack=nfs-client-${RANDOM}
     rio up $nfs_server_stack ./tests/nfs-stack/nfs-server-stack.yaml
     rio wait ${nfs_server_stack}/nfs-server
-    ip=$(rio kubectl get po --all-namespaces -o wide | grep nfs-server | awk '{print $(NF-1)}')
+    ip=$(rio kubectl get po --all-namespaces -o wide | grep nfs-server | awk '{print $(NF-2)}')
     printf "NFS_SERVER_HOSTNAME: $ip\nNFS_SERVER_EXPORT_PATH: /" > answers.yaml
     rio up --answers answers.yaml $nfs_client_stack ./tests/nfs-stack/nfs-client-stack.yaml
     rio wait ${nfs_client_stack}/nfs-provisioner
