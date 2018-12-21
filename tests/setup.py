a2cbf79b5229a8baa9f5e516da942ee0f9ada881
Darren Shepherd <darren@rancher.com>
2018-12-21 13:30:59 -0700

    Enable python tests

---
+++ tests/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='IntegrationTests',
+    version='0.1',
+    packages=[
+    ],
+    license='ASL 2.0',
+    long_description=open('README.txt').read(),
+)
