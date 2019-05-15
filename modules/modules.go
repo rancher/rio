d60308f25a23158fef9ff574263b8c8286f16c68
Darren Shepherd <darren@rancher.com>
2019-05-14 20:01:03 -0700

    Remove storage features

--- modules/modules.go
+++ modules/modules.go
@@ -8,16 +8,12 @@ import (
 	"github.com/rancher/rio/modules/istio"
 	"github.com/rancher/rio/modules/monitoring"
 	"github.com/rancher/rio/modules/service"
-	"github.com/rancher/rio/modules/storage"
 	"github.com/rancher/rio/modules/system"
 	"github.com/rancher/rio/types"
 )
 
 func Register(ctx context.Context, rioContext *types.Context) error {
 	if err := system.Register(ctx, rioContext); err != nil {
-		return err
-	}
-	if err := storage.Register(ctx, rioContext); err != nil {
 		return err
 	}
 	if err := service.Register(ctx, rioContext); err != nil {
