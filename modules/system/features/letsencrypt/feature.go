72905b3fcc5062388f964e6d878856cbc512305c
Darren Shepherd <darren@rancher.com>
2019-05-21 12:27:59 -0700

    Switch to staging certificate names

--- modules/system/features/letsencrypt/feature.go
+++ modules/system/features/letsencrypt/feature.go
@@ -37,8 +37,8 @@ func Register(ctx context.Context, rContext *types.Context) error {
 				},
 			},
 			Answers: map[string]string{
-				constants.RioWildcardType:  constants.ProductionType,
-				constants.PublicDomainType: constants.ProductionType,
+				constants.RioWildcardType:  constants.StagingType,
+				constants.PublicDomainType: constants.StagingType,
 			},
 		},
 		FixedAnswers: map[string]string{
