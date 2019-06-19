6772790034c8baa5d00b40c2e7d4111100d50d6f
Daishan Peng <daishan@rancher.com>
2019-06-18 17:59:41 -0700

    Add socat to registry proxy

--- modules/build/controllers/proxy/controller.go
+++ modules/build/controllers/proxy/controller.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/rancher/rio/pkg/constructors"
 	"github.com/rancher/rio/types"
@@ -14,6 +15,7 @@ import (
 
 var (
 	proxyImage = "rancher/klipper-lb:v0.1.2"
+	socatImage = "alpine/socat:1.0.3"
 	T          = true
 )
 
@@ -97,7 +99,51 @@ func (h *Handler) onChange(key string, svc *corev1.Service) (*corev1.Service, er
 		},
 	})
 
+	socatDeploy := constructors.NewDaemonset(svc.Namespace, "socat", v1.Deployment{
+		ObjectMeta: v12.ObjectMeta{
+			OwnerReferences: []v12.OwnerReference{
+				{
+					Name:       svc.Name,
+					UID:        svc.UID,
+					Kind:       "Service",
+					APIVersion: "v1",
+				},
+			},
+		},
+		Spec: v1.DeploymentSpec{
+			Selector: &v12.LabelSelector{
+				MatchLabels: map[string]string{
+					"app": "socat",
+				},
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: v12.ObjectMeta{
+					Labels: map[string]string{
+						"app": "socat",
+					},
+				},
+				Spec: corev1.PodSpec{
+					HostNetwork: true,
+					Containers: []corev1.Container{
+						{
+							Name:  "socat",
+							Image: socatImage,
+							Command: []string{
+								"socat",
+								"TCP-LISTEN:5442,fork,bind=127.0.0.1,reuseaddr",
+								fmt.Sprintf("TCP:%s:80", svc.Spec.ClusterIP),
+							},
+							SecurityContext: &corev1.SecurityContext{
+								Privileged: &T,
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+
 	os := objectset.NewObjectSet()
-	os.Add(deploy)
+	os.Add(deploy, socatDeploy)
 	return svc, h.apply.WithSetID(svc.Name + "-proxy").WithOwner(svc).Apply(os)
 }
