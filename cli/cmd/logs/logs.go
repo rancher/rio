24db142e4c1f0a591fbdd574302533f6b7d6e29d
Caleb Bron <cbron@users.noreply.github.com>
2019-09-17 14:00:07 -0700

    Add timestamps to logs with flag

--- cli/cmd/logs/logs.go
+++ cli/cmd/logs/logs.go
@@ -17,13 +17,14 @@ import (
 )
 
 type Logs struct {
-	F_Follow    bool   `desc:"Follow log output"`
-	S_Since     string `desc:"Logs since a certain time, either duration (5s, 2m, 3h) or RFC3339"`
-	P_Previous  bool   `desc:"Print the logs for the previous instance of the container in a pod if it exists"`
-	C_Container string `desc:"Print the logs of a specific container"`
-	R_Revision  string `desc:"Print the logs of a specific revision"`
-	N_Tail      int    `desc:"Number of recent lines of logs to print, -1 for all" default:"200"`
-	A_All       bool   `desc:"Include hidden or systems logs when logging"`
+	F_Follow     bool   `desc:"Follow log output"`
+	S_Since      string `desc:"Logs since a certain time, either duration (5s, 2m, 3h) or RFC3339"`
+	P_Previous   bool   `desc:"Print the logs for the previous instance of the container in a pod if it exists"`
+	C_Container  string `desc:"Print the logs of a specific container"`
+	R_Revision   string `desc:"Print the logs of a specific revision"`
+	N_Tail       int    `desc:"Number of recent lines of logs to print, -1 for all" default:"200"`
+	A_All        bool   `desc:"Include hidden or systems logs when logging"`
+	T_Timestamps bool   `desc:"Print the logs with timestamp"`
 }
 
 func (l *Logs) Run(ctx *clicontext.CLIContext) error {
@@ -78,6 +79,9 @@ func (l *Logs) logContainer(pod *v1.Pod, container v1.Container, coreClient core
 	podLogOption := &v1.PodLogOptions{
 		Container: container.Name,
 		Follow:    l.F_Follow,
+	}
+	if l.T_Timestamps {
+		podLogOption.Timestamps = true
 	}
 	if l.S_Since != "" {
 		t, err := time.Parse(time.RFC3339, l.S_Since)
