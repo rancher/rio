dfe3ff8ca28f0c0ae52e2201a50e356773f20f55
Caleb Bron <cbron@users.noreply.github.com>
2019-10-25 12:53:32 -0700

    continue through install

--- pkg/controllers/controllers.go
+++ pkg/controllers/controllers.go
@@ -4,14 +4,10 @@ import (
 	"context"
 
 	"github.com/rancher/rio/pkg/controllers/config"
-	"github.com/rancher/rio/pkg/controllers/systemstatus"
 	"github.com/rancher/rio/types"
 )
 
 func Register(ctx context.Context, rContext *types.Context) error {
 	// Controllers
-	if err := config.Register(ctx, rContext); err != nil {
-		return err
-	}
-	return systemstatus.Register(ctx, rContext)
+	return config.Register(ctx, rContext)
 }
