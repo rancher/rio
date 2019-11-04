c5bd58ebb48f503271de5a93525a25673b8eff16
Darren Shepherd <darren@rancher.com>
2019-11-03 23:54:57 -0700

    Update vendor

--- vendor/github.com/rancher/wrangler/pkg/apply/reconcilers.go
+++ vendor/github.com/rancher/wrangler/pkg/apply/reconcilers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 
+	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
@@ -15,10 +16,58 @@ import (
 
 var (
 	defaultReconcilers = map[schema.GroupVersionKind]Reconciler{
-		v1.SchemeGroupVersion.WithKind("Service"):  reconcileService,
-		batchv1.SchemeGroupVersion.WithKind("Job"): reconcileJob,
+		v1.SchemeGroupVersion.WithKind("Service"):        reconcileService,
+		batchv1.SchemeGroupVersion.WithKind("Job"):       reconcileJob,
+		appsv1.SchemeGroupVersion.WithKind("Deployment"): reconcileDeployment,
+		appsv1.SchemeGroupVersion.WithKind("DaemonSet"):  reconcileDaemonSet,
 	}
 )
+
+func reconcileDaemonSet(oldObj, newObj runtime.Object) (bool, error) {
+	oldSvc, ok := oldObj.(*appsv1.DaemonSet)
+	if !ok {
+		oldSvc = &appsv1.DaemonSet{}
+		if err := convertObj(oldObj, oldSvc); err != nil {
+			return false, err
+		}
+	}
+	newSvc, ok := newObj.(*appsv1.DaemonSet)
+	if !ok {
+		newSvc = &appsv1.DaemonSet{}
+		if err := convertObj(newObj, newSvc); err != nil {
+			return false, err
+		}
+	}
+
+	if !equality.Semantic.DeepEqual(oldSvc.Spec.Selector, newSvc.Spec.Selector) {
+		return false, ErrReplace
+	}
+
+	return false, nil
+}
+
+func reconcileDeployment(oldObj, newObj runtime.Object) (bool, error) {
+	oldSvc, ok := oldObj.(*appsv1.Deployment)
+	if !ok {
+		oldSvc = &appsv1.Deployment{}
+		if err := convertObj(oldObj, oldSvc); err != nil {
+			return false, err
+		}
+	}
+	newSvc, ok := newObj.(*appsv1.Deployment)
+	if !ok {
+		newSvc = &appsv1.Deployment{}
+		if err := convertObj(newObj, newSvc); err != nil {
+			return false, err
+		}
+	}
+
+	if !equality.Semantic.DeepEqual(oldSvc.Spec.Selector, newSvc.Spec.Selector) {
+		return false, ErrReplace
+	}
+
+	return false, nil
+}
 
 func reconcileService(oldObj, newObj runtime.Object) (bool, error) {
 	oldSvc, ok := oldObj.(*v1.Service)
@@ -36,7 +85,7 @@ func reconcileService(oldObj, newObj runtime.Object) (bool, error) {
 		}
 	}
 
-	if newSvc.Spec.Type != "" && newSvc.Spec.Type != newSvc.Spec.Type {
+	if newSvc.Spec.Type != "" && oldSvc.Spec.Type != newSvc.Spec.Type {
 		return false, ErrReplace
 	}
 
