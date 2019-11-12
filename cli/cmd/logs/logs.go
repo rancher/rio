6c325a864ea43150e8712a640066f810d639d9ce
Caleb Bron <cbron@users.noreply.github.com>
2019-11-12 16:39:41 -0700

    alias logs cmd

--- cli/cmd/logs/logs.go
+++ cli/cmd/logs/logs.go
@@ -14,6 +14,7 @@ import (
 	"github.com/rancher/rio/cli/cmd/util"
 	"github.com/rancher/rio/cli/pkg/clicontext"
 	clitypes "github.com/rancher/rio/cli/pkg/types"
+	"github.com/urfave/cli"
 	"github.com/wercker/stern/stern"
 	"k8s.io/apimachinery/pkg/labels"
 )
@@ -30,6 +31,10 @@ type Logs struct {
 }
 
 // This is based on both wercker/stern and linkerd/stern implementations
+
+func (l *Logs) Customize(cmd *cli.Command) {
+	cmd.ShortName = "log"
+}
 
 func (l *Logs) Run(ctx *clicontext.CLIContext) error {
 	conf, err := l.setupConfig(ctx)
