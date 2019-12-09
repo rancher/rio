6cac66d3f5c1b8d461550a53ba173c3474d4ebf0
Daishan <daishan@rancher.com>
2019-12-09 14:23:04 -0700

    Fix target can't be found if target is app

--- cli/cmd/publicdomain/add.go
+++ cli/cmd/publicdomain/add.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/rancher/rio/pkg/services"
-
 	"github.com/rancher/rio/cli/pkg/clicontext"
 	adminv1 "github.com/rancher/rio/pkg/apis/admin.rio.cattle.io/v1"
 	riov1 "github.com/rancher/rio/pkg/apis/rio.cattle.io/v1"
+	"github.com/rancher/rio/pkg/services"
+	"github.com/rancher/wrangler/pkg/kv"
 )
 
 type Register struct {
@@ -22,30 +22,41 @@ func (r *Register) Run(ctx *clicontext.CLIContext) error {
 	domainName := ctx.CLI.Args().Get(0)
 	target := ctx.CLI.Args().Get(1)
 
-	result, err := ctx.ByID(target)
-	if err != nil {
-		return err
-	}
-
 	pd := adminv1.PublicDomain{
 		Spec: adminv1.PublicDomainSpec{
 			SecretName: r.Secret,
 		},
 	}
-	switch result.Object.(type) {
-	case *riov1.Service:
-		svc := result.Object.(*riov1.Service)
-		targetApp, targetVersion := services.AppAndVersion(svc)
-		if !strings.Contains(target, "@") {
-			targetVersion = ""
+
+	/*
+		1) if target is [namespace:]name and can't be found as an object, treat it as app
+		2) if target is [namespace:]name@version, treat it as service(revision)
+		3) if target is [namespace:]name and can be found as an object, treat it as a router
+	*/
+	result, err := ctx.ByID(target)
+	if err != nil {
+		ns, app := kv.RSplit(target, ":")
+		if ns == "" {
+			ns = ctx.GetSetNamespace()
 		}
-		pd.Spec.TargetApp = targetApp
-		pd.Spec.TargetVersion = targetVersion
-		pd.Spec.TargetNamespace = svc.Namespace
-	case *riov1.Router:
-		router := result.Object.(*riov1.Router)
-		pd.Spec.TargetRouter = router.Name
-		pd.Spec.TargetNamespace = router.Namespace
+		pd.Spec.TargetApp = app
+		pd.Spec.TargetNamespace = ns
+	} else {
+		switch result.Object.(type) {
+		case *riov1.Service:
+			svc := result.Object.(*riov1.Service)
+			targetApp, targetVersion := services.AppAndVersion(svc)
+			if !strings.Contains(target, "@") {
+				targetVersion = ""
+			}
+			pd.Spec.TargetApp = targetApp
+			pd.Spec.TargetVersion = targetVersion
+			pd.Spec.TargetNamespace = svc.Namespace
+		case *riov1.Router:
+			router := result.Object.(*riov1.Router)
+			pd.Spec.TargetRouter = router.Name
+			pd.Spec.TargetNamespace = router.Namespace
+		}
 	}
 
 	return ctx.Create(adminv1.NewPublicDomain(pd.Spec.TargetNamespace, domainName, pd))
